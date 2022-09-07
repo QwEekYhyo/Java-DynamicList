7328fae2622c5534d2df516c09cb0b7a12461660
QwEekYhyo <qweekyhyo.yt83@gmail.com>
2022-09-07 18:45:44 +0200

    Adding files

---
+++ DynamicList.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class DynamicList {
+    private int capacity;
+    private int size;
+    private int[] list;
+
+    public DynamicList(int capac) {
+        this.capacity = capac;
+        this.size = 0;
+        this.list = new int[this.capacity];
+    }
+
+    private void ensure() {
+        if (this.size >= this.capacity) {
+            int[] n = new int[this.capacity+1];
+            for (int i = 0; i<this.size; i++) {n[i] = this.list[i];}
+            this.list = n;
+            this.capacity++;
+        }
+    }
+
+    public void append(int x) {
+        this.ensure();
+        this.list[this.size] = x;
+        this.size++;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(this.list);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        DynamicList array = new DynamicList(3);
+        for (int i = 0; i<10; i++) {
+            System.out.println(array.toString());
+            array.append((i - 3) * (i - 3));
+        }
+    }
+}
